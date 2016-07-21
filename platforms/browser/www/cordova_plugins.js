659c22c4734dbc4ea62867850e6e865c546781d6
Jose D. Villareal <jose@practisis.com>
2016-07-21 12:50:53 -0500

    Listo todo
    Agreo un nuevo proyecto al nuevo

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/nirwan.cordova.plugin.printer/www/printer.js",
+        "id": "nirwan.cordova.plugin.printer.Printer",
+        "pluginId": "nirwan.cordova.plugin.printer",
+        "clobbers": [
+            "plugin.printer"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "nirwan.cordova.plugin.printer": "0.6.0dev"
+}
+// BOTTOM OF METADATA
+});
